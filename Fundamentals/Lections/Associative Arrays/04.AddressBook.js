3e8f5964c890f1d7eb7560351c74959b16cf00ad
Rosen Koychev <95128567+RoKoBG@users.noreply.github.com>
2022-07-26 18:39:49 +0300

    Update 04.AddressBook.js

--- Fundamentals/Lections/Associative Arrays/04.AddressBook.js
+++ Fundamentals/Lections/Associative Arrays/04.AddressBook.js
@@ -4,10 +4,19 @@ function addressBook(data) {
         let [name, address] = line.split(":");
         books[name] = address;
     }
+
+
+
     let entries = Object.entries(books);
     let sortEntries= entries.sort(([keyA,valueA],[keyB,valueB]) =>{
         return keyA.localeCompare(keyB);
     })
+
+    //  Other way of sorting only the keys.
+    
+    //let unsortedEntries= Object.keys(books);
+    //let sortedEntries = unsortedEntries.sort((a,b) => a.localeCompare(b));
+    
     for(let [name, address] of sortEntries){
         console.log(`${name} -> ${address}`);
     }
