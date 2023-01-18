3ce87cdd7287cedf4bd590bfd15c4d156fea39c4
Rosen Koychev <rosen.koichev@gmail.com>
2023-01-18 05:33:20 +0200

    Add files via upload

--- Advanced/Arrays and Nested Arrays - Lab/09.BiggestEle.js
+++ Advanced/Arrays and Nested Arrays - Lab/09.BiggestEle.js
@@ -1,1 +1,16 @@
-func
+function biggestElement(matrix) {
+ 
+    let biggestNum = Number.NEGATIVE_INFINITY;
+    matrix.forEach(
+            row => row.forEach(
+                col => biggestNum = Math.max(biggestNum, col)));
+    console.log(biggestNum);
+}
+ 
+ 
+biggestElement([
+        [3, 5, 7, 12],
+        [-1, 4, 33, 2],
+        [8, 3, 0, 4]
+    ]
+);
