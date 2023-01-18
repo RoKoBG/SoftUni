5b9857621dfb1626cbc44b7d1e4de285ffdd6ae3
Rosen Koychev <rosen.koichev@gmail.com>
2023-01-18 05:26:47 +0200

    Delete Arrays and Nested Arrays - Lab directory

--- Arrays and Nested Arrays - Lab/03.SumFirstLast.js
+++
@@ -1,9 +0,0 @@
-function sumFirstLast(arr) {
-    const first = Number(arr.shift());
-    const last = Number(arr.pop());
-    const sum = first + last;
-    console.log(sum);
-    
-}
-sumFirstLast(['20', '30', '40']);
-sumFirstLast(['5', '10']);
