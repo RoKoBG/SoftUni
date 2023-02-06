3ff8d1b7a91aab0f49d555a624d20cbd81d6c614
Rosen Koychev <rosen.koichev@gmail.com>
2023-02-06 18:56:11 +0200

    Delete Advanced/DOM Manipulations and Events - Lab directory

--- Advanced/DOM Manipulations and Events - Lab/01. List-Of-Items/app.js
+++
@@ -1,8 +0,0 @@
-function addItem() {
-    const input = document.getElementById('newItemText').value;
-    const li=document.createElement('li');
-    li.textContent=input;
-    const list = document.getElementById('items');
-    list.appendChild(li);
-
-}
